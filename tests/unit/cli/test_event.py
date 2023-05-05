306798c726f7cdd2bc16abfb7f4f5ef6d786aa73
Cyber OS Lab <99838653+cyberoslab@users.noreply.github.com>
2023-05-04 19:54:38 -0700

    Update test_event.py
    Signed-off-by: Cyber OS Lab. <99838653+cyberoslab@users.noreply.github.com>

--- tests/unit/cli/test_event.py
+++ tests/unit/cli/test_event.py
@@ -1,283 +1,45 @@
 import pytest
 
-
-@pytest.fixture
-def payload():
-    return {
-        "imageDigest": "sha256:285bc3161133ec01d8ca8680cd746eecbfdbc1faa6313bd863151c4b26d7e5a5",
-        "vulnerabilities": [
-            {
-                "feed": "vulnerabilities",
-                "feed_group": "centos:7",
-                "fix": "0:3.44.0-7.el7_7",
-                "nvd_data": [
-                    {
-                        "cvss_v2": {
-                            "base_score": 5.0,
-                            "exploitability_score": 10.0,
-                            "impact_score": 2.9,
-                        },
-                        "cvss_v3": {
-                            "base_score": 7.5,
-                            "exploitability_score": 3.9,
-                            "impact_score": 3.6,
-                        },
-                        "id": "CVE-2019-11729",
-                    },
-                    {
-                        "cvss_v2": {
-                            "base_score": 6.8,
-                            "exploitability_score": 8.6,
-                            "impact_score": 6.4,
-                        },
-                        "cvss_v3": {
-                            "base_score": -1.0,
-                            "exploitability_score": -1.0,
-                            "impact_score": -1.0,
-                        },
-                        "id": "CVE-2019-11745",
-                    },
-                ],
-                "package": "nss-3.44.0-4.el7",
-                "package_cpe": "None",
-                "package_cpe23": "None",
-                "package_name": "nss",
-                "package_path": "None",
-                "package_type": "rpm",
-                "package_version": "3.44.0-4.el7",
-                "severity": "High",
-                "url": "https://access.redhat.com/errata/RHSA-2019:4190",
-                "vendor_data": [],
-                "vuln": "RHSA-2019:4190",
-            },
-            {
-                "feed": "vulnerabilities",
-                "feed_group": "centos:7",
-                "fix": "0:3.44.0-7.el7_7",
-                "nvd_data": [
-                    {
-                        "cvss_v2": {
-                            "base_score": 5.0,
-                            "exploitability_score": 10.0,
-                            "impact_score": 2.9,
-                        },
-                        "cvss_v3": {
-                            "base_score": 7.5,
-                            "exploitability_score": 3.9,
-                            "impact_score": 3.6,
-                        },
-                        "id": "CVE-2019-11729",
-                    },
-                    {
-                        "cvss_v2": {
-                            "base_score": 6.8,
-                            "exploitability_score": 8.6,
-                            "impact_score": 6.4,
-                        },
-                        "cvss_v3": {
-                            "base_score": -1.0,
-                            "exploitability_score": -1.0,
-                            "impact_score": -1.0,
-                        },
-                        "id": "CVE-2019-11745",
-                    },
-                ],
-                "package": "nss-sysinit-3.44.0-4.el7",
-                "package_cpe": "None",
-                "package_cpe23": "None",
-                "package_name": "nss-sysinit",
-                "package_path": "None",
-                "package_type": "rpm",
-                "package_version": "3.44.0-4.el7",
-                "severity": "High",
-                "url": "https://access.redhat.com/errata/RHSA-2019:4190",
-                "vendor_data": [],
-                "vuln": "RHSA-2019:4190",
-            },
-            {
-                "feed": "vulnerabilities",
-                "feed_group": "centos:7",
-                "fix": "0:3.44.0-4.el7_7",
-                "nvd_data": [
-                    {
-                        "cvss_v2": {
-                            "base_score": 5.0,
-                            "exploitability_score": 10.0,
-                            "impact_score": 2.9,
-                        },
-                        "cvss_v3": {
-                            "base_score": 7.5,
-                            "exploitability_score": 3.9,
-                            "impact_score": 3.6,
-                        },
-                    },
-                    {
-                        "cvss_v2": {
-                            "base_score": 6.8,
-                            "exploitability_score": 8.6,
-                            "impact_score": 6.4,
-                        },
-                        "cvss_v3": {
-                            "base_score": -1.0,
-                            "exploitability_score": -1.0,
-                            "impact_score": -1.0,
-                        },
-                        "id": "CVE-2019-11745",
-                    },
-                ],
-                "package": "nss-util-3.44.0-3.el7",
-                "package_cpe": "None",
-                "package_cpe23": "None",
-                "package_name": "nss-util",
-                "package_path": "None",
-                "package_type": "rpm",
-                "package_version": "3.44.0-3.el7",
-                "severity": "High",
-                "url": "https://access.redhat.com/errata/RHSA-2019:4190",
-                "vendor_data": [],
-                "vuln": "RHSA-2019:4190",
-            },
-        ],
-        "vulnerability_type": "os",
-    import pytest
+from nextlinuxcli.cli import event
+from nextlinuxcli.clients import apiexternal
+from click.testing import CliRunner
 
 
-@pytest.fixture
-def payload():
-    return {
-        "imageDigest": "sha256:285bc3161133ec01d8ca8680cd746eecbfdbc1faa6313bd863151c4b26d7e5a5",
-        "vulnerabilities": [
-            {
-                "feed": "vulnerabilities",
-                "feed_group": "centos:7",
-                "fix": "0:3.44.0-7.el7_7",
-                "nvd_data": [
-                    {
-                        "cvss_v2": {
-                            "base_score": 5.0,
-                            "exploitability_score": 10.0,
-                            "impact_score": 2.9,
-                        },
-                        "cvss_v3": {
-                            "base_score": 7.5,
-                            "exploitability_score": 3.9,
-                            "impact_score": 3.6,
-                        },
-                        "id": "CVE-2019-11729",
-                    },
-                    {
-                        "cvss_v2": {
-                            "base_score": 6.8,
-                            "exploitability_score": 8.6,
-                            "impact_score": 6.4,
-                        },
-                        "cvss_v3": {
-                            "base_score": -1.0,
-                            "exploitability_score": -1.0,
-                            "impact_score": -1.0,
-                        },
-                        "id": "CVE-2019-11745",
-                    },
-                ],
-                "package": "nss-3.44.0-4.el7",
-                "package_cpe": "None",
-                "package_cpe23": "None",
-                "package_name": "nss",
-                "package_path": "None",
-                "package_type": "rpm",
-                "package_version": "3.44.0-4.el7",
-                "severity": "High",
-                "url": "https://access.redhat.com/errata/RHSA-2019:4190",
-                "vendor_data": [],
-                "vuln": "RHSA-2019:4190",
-            },
-            {
-                "feed": "vulnerabilities",
-                "feed_group": "centos:7",
-                "fix": "0:3.44.0-7.el7_7",
-                "nvd_data": [
-                    {
-                        "cvss_v2": {
-                            "base_score": 5.0,
-                            "exploitability_score": 10.0,
-                            "impact_score": 2.9,
-                        },
-                        "cvss_v3": {
-                            "base_score": 7.5,
-                            "exploitability_score": 3.9,
-                            "impact_score": 3.6,
-                        },
-                        "id": "CVE-2019-11729",
-                    },
-                    {
-                        "cvss_v2": {
-                            "base_score": 6.8,
-                            "exploitability_score": 8.6,
-                            "impact_score": 6.4,
-                        },
-                        "cvss_v3": {
-                            "base_score": -1.0,
-                            "exploitability_score": -1.0,
-                            "impact_score": -1.0,
-                        },
-                        "id": "CVE-2019-11745",
-                    },
-                ],
-                "package": "nss-sysinit-3.44.0-4.el7",
-                "package_cpe": "None",
-                "package_cpe23": "None",
-                "package_name": "nss-sysinit",
-                "package_path": "None",
-                "package_type": "rpm",
-                "package_version": "3.44.0-4.el7",
-                "severity": "High",
-                "url": "https://access.redhat.com/errata/RHSA-2019:4190",
-                "vendor_data": [],
-                "vuln": "RHSA-2019:4190",
-            },
-            {
-                "feed": "vulnerabilities",
-                "feed_group": "centos:7",
-                "fix": "0:3.44.0-4.el7_7",
-                "nvd_data": [
-                    {
-                        "cvss_v2": {
-                            "base_score": 5.0,
-                            "exploitability_score": 10.0,
-                            "impact_score": 2.9,
-                        },
-                        "cvss_v3": {
-                            "base_score": 7.5,
-                            "exploitability_score": 3.9,
-                            "impact_score": 3.6,
-                        },
-                    },
-                    {
-                        "cvss_v2": {
-                            "base_score": 6.8,
-                            "exploitability_score": 8.6,
-                            "impact_score": 6.4,
-                        },
-                        "cvss_v3": {
-                            "base_score": -1.0,
-                            "exploitability_score": -1.0,
-                            "impact_score": -1.0,
-                        },
-                        "id": "CVE-2019-11745",
-                    },
-                ],
-                "package": "nss-util-3.44.0-3.el7",
-                "package_cpe": "None",
-                "package_cpe23": "None",
-                "package_name": "nss-util",
-                "package_path": "None",
-                "package_type": "rpm",
-                "package_version": "3.44.0-3.el7",
-                "severity": "High",
-                "url": "https://access.redhat.com/errata/RHSA-2019:4190",
-                "vendor_data": [],
-                "vuln": "RHSA-2019:4190",
-            },
-        ],
-        "vulnerability_type": "os",
-    }}
+@pytest.mark.parametrize(
+    "input_level, expected_level, expected_code",
+    [
+        ("info", "info", None),
+        ("INFO", "info", None),
+        ("error", "error", None),
+        ("ERROR", "error", None),
+        ("other", None, 1),
+    ],
+)
+def test_list_normalize_level(monkeypatch, input_level, expected_level, expected_code):
+    normalized_level = []
+
+    def mock_method(
+        config,
+        since=None,
+        before=None,
+        level=None,
+        service=None,
+        host=None,
+        resource=None,
+        resource_type=None,
+        event_type=None,
+        all=False,
+    ):
+        normalized_level.append(level)
+
+    monkeypatch.setattr(
+        apiexternal,
+        "list_events",
+        mock_method,
+    )
+    runner = CliRunner()
+    result = runner.invoke(event.list, ["--level", input_level])
+    if expected_code is not None:
+        assert result.exit_code == expected_code
+    else:
+        assert normalized_level == [expected_level]
